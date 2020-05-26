44b8b806ac8f098695f501538f279a1480e89c43
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-05-26 16:41:13 -0300

    fix: fix docker

--- backend/app/UpdateEmailRequest.php
+++ backend/app/UpdateEmailRequest.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ChangeEmailRequest extends Model
+class UpdateEmailRequest extends Model
 {
     //
 }
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,1 +1,3 @@
 php artisan migrate
+
+php-fpm
