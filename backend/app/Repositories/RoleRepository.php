1d4cc7ef470980d701cfbc21937c5136f5411362
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-05-28 20:02:55 -0300

    feat: add rolescontroller

---
+++ backend/app/Repositories/RoleRepository.php
@@ -0,0 +1,93 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Role;
+use App\User;
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Model;
+
+class RoleRepository
+{
+
+  const CACHE_KEY = 'roles';
+
+  private CacheRepository $cacheRepository;
+
+  /**
+   * RoleRepository constructor
+   *
+   * @param CacheRepository $cacheRepository
+   */
+  public function __construct(CacheRepository $cacheRepository)
+  {
+    $this->cacheRepository = $cacheRepository;
+  }
+
+  /**
+   * Find all roles in a page
+   *
+   * @param int $page
+   * @return LengthAwarePaginator
+   */
+  public function findAllRolesInPage($page)
+  {
+    return Role::query()->paginate();
+  }
+
+  /**
+   * Find all user's roles in a page
+   *
+   * @param User $user
+   * @param int $page
+   * @return LengthAwarePaginator
+   */
+  public function findAllRolesOfUserInPage($user, $page)
+  {
+    return $user->roles()->paginate();
+  }
+
+  /**
+   * Store role in database
+   *
+   * @param array $data
+   * @return Model
+   */
+  public function store(array $data)
+  {
+    return Role::create($data);
+  }
+
+  /**
+   * Find role by it's id
+   *
+   * @param int $id
+   * @return User
+   */
+  public function findRoleById($id)
+  {
+    return $this->cacheRepository->remember($this->getCacheKey("show.$id"), now()->addHour(), function () use ($id) {
+      return Role::findOrFail($id);
+    });
+  }
+
+  /**
+   * Forget role from cache
+   *
+   * @param Role $role
+   * @return bool
+   */
+  public function forgetRoleFromCache($role)
+  {
+    return $this->cacheRepository->forget($this->getCacheKey("show.{$role->id}"));
+  }
+
+  public final function getCacheKey($key)
+  {
+    return self::CACHE_KEY . '.' . $key;
+  }
+
+}
