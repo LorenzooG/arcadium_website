03e794d290c24571e7da6f52e79d46f8da3fdd87
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-05-28 21:18:59 -0300

    feat: role policy

---
+++ backend/app/Policies/RolePolicy.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Policies;
+
+use App\Role;
+use App\User;
+use App\Utils\Permission;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RolePolicy
+{
+  use HandlesAuthorization;
+
+  /**
+   * Determine whether the user can view any models.
+   *
+   * @param User $user
+   * @return mixed
+   */
+  public function viewAny(User $user)
+  {
+    return $user->hasPermission(Permission::VIEW_ANY_ROLE);
+  }
+
+  /**
+   * Determine whether the user can view the model.
+   *
+   * @param User $user
+   * @param Role $role
+   * @return mixed
+   */
+  public function view(User $user, Role $role)
+  {
+    return $user->hasPermission(Permission::VIEW_ROLE)
+      && $user->roles()->where('role_id', $role->id)->exists();
+  }
+
+  /**
+   * Determine whether the user can create models.
+   *
+   * @param User $user
+   * @return mixed
+   */
+  public function create(User $user)
+  {
+    return $user->hasPermission(Permission::STORE_ROLE);
+  }
+
+  /**
+   * Determine whether the user can update the model.
+   *
+   * @param User $user
+   * @return mixed
+   */
+  public function update(User $user)
+  {
+    return $user->hasPermission(Permission::UPDATE_ROLE);
+  }
+
+  /**
+   * Determine whether the user can delete the model.
+   *
+   * @param User $user
+   * @return mixed
+   */
+  public function delete(User $user)
+  {
+    return $user->hasPermission(Permission::DELETE_ROLE);
+  }
+}
