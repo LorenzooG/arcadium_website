c6b63ef8cbf062a59daa585a650f7eee2067a3de
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-07 03:49:03 -0300

    feat: implement /user/posts and /user/roles
    fix: decode html special chars in User set password

--- backend/app/Http/Controllers/SelfUserController.php
+++ backend/app/Http/Controllers/SelfUserController.php
@@ -7,11 +7,58 @@ use App\Http\Requests\UserDeleteRequest;
 use App\Http\Requests\UserUpdateEmailRequest;
 use App\Http\Requests\UserUpdatePasswordRequest;
 use App\Http\Requests\UserUpdateRequest;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\RoleResource;
+use App\Repositories\PostRepository;
+use App\Repositories\RoleRepository;
 use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
+use Illuminate\Pagination\Paginator;
 
 class SelfUserController extends Controller
 {
+
+	private PostRepository $postRepository;
+	private RoleRepository $roleRepository;
+	
+	/**
+	 * SelfUserController constructor
+	 *
+	 * @param PostRepository $postRepository
+	 * @param RoleRepository $roleRepository
+	 */
+	public function __construct(PostRepository $postRepository, RoleRepository $roleRepository)
+	{
+		$this->postRepository = $postRepository;
+		$this->roleRepository = $roleRepository;
+	}
+
+	/**
+	 * Show the current user's posts
+	 *
+	 * @return AnonymousResourceCollection
+	 */
+	public function roles(Request $request)
+	{
+		$page = Paginator::resolveCurrentPage();
+
+		return RoleResource::collection($this->roleRepository->findPaginatedRolesForUser($request->user(), $page));
+	}
+
+	/**
+	 * Show the current user's roles
+	 *
+	 * @return AnonymousResourceCollection
+	 */
+	public function posts(Request $request)
+	{
+		$page = Paginator::resolveCurrentPage();
+
+		return PostResource::collection($this->postRepository->findPaginatedPostsForUser($request->user(), $page));
+	}
+
   /**
    * Update current user's name and user name
    *
