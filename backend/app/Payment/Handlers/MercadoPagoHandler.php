c21e1ea7f66c77e5646d64ee513f4f0301405aa2
= <lorenzooguimaraes@gmail.com>
2020-06-17 06:15:08 -0300

    feat: checkout mercado pago

--- backend/app/Payment/Handlers/MercadoPagoHandler.php
+++ backend/app/Payment/Handlers/MercadoPagoHandler.php
@@ -3,10 +3,21 @@
 
 namespace App\Payment\Handlers;
 
+use App\Payment;
 use App\Payment\Contracts\PaymentHandlerContract;
+use App\Product;
+use App\Repositories\PaymentRepository;
+use App\Repositories\ProductRepository;
 use App\User;
+use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use MercadoPago\Item as MercadoPagoItem;
+use MercadoPago\MerchantOrder;
+use MercadoPago\Payer as MercadoPagoPayer;
+use MercadoPago\Payment as MercadoPagoPayment;
+use MercadoPago\Preference as MercadoPagoPreference;
+use MercadoPago\SDK as MercadoPagoSDK;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class MercadoPagoHandler
@@ -19,29 +30,107 @@ final class MercadoPagoHandler implements PaymentHandlerContract
 {
   const KEY = 'MERCADO_PAGO';
 
-  public final function __construct()
+  private ProductRepository $productRepository;
+  private PaymentRepository $paymentRepository;
+
+  private string $notificationUrl;
+
+  /**
+   * MercadoPagoHandler constructor
+   *
+   * @param PaymentRepository $paymentRepository
+   * @param ProductRepository $productRepository
+   */
+  public final function __construct(PaymentRepository $paymentRepository, ProductRepository $productRepository)
   {
+    $this->productRepository = $productRepository;
+    $this->paymentRepository = $paymentRepository;
+
+    $this->notificationUrl = route('payments.notification', [
+      'paymentHandler' => self::KEY
+    ]);
+
     $this->setupCredentials();
   }
 
   public function setupCredentials(): void
   {
-    // TODO: Implement setupCredentials() method.
+    MercadoPagoSDK::setAccessToken(config('app.mp_access_token'));
+    MercadoPagoSDK::setClientId(config('app.mp_client_id'));
+    MercadoPagoSDK::setClientSecret(config('app.mp_client_secret'));
   }
 
-  public function findMerchantOrderById($id)
+  public function findMerchantOrderById($id): MerchantOrder
   {
-    // TODO: Implement findMerchantOrderById() method.
+    return MerchantOrder::find_by_id($id);
   }
 
-  public function findItemById($id)
+  public function findItemById($id): MercadoPagoPayment
   {
-    // TODO: Implement findItemById() method.
+    return MercadoPagoPayment::find_by_id($id);
   }
 
-  public function handleCheckout(User $user, array $items): Response
+  /**
+   * Handle payment checkout
+   *
+   * @param User $user
+   * @param string $userName
+   * @param string $originIpAddress
+   * @param array $items
+   * @return Response
+   * @throws Exception
+   */
+  public function handleCheckout(User $user, string $userName, string $originIpAddress, array $items): Response
   {
-    // TODO: Implement handleCheckout() method.
+    $totalPrice = 0;
+    $mercadoPagoPreferenceItems = [];
+
+    foreach ($items as $item) {
+      $item['product'] = ($product = $this->productRepository->findProductById($item['product']));
+
+      $totalPrice += $product->price * $item['amount'];
+    }
+
+    /** @var Payment $payment */
+    $payment = $user->payments()->create([
+      'payment_method' => self::KEY,
+      'user_name' => $userName,
+      'total_price' => $totalPrice,
+      'origin_ip_address' => $originIpAddress,
+    ]);
+
+    foreach ($items as $item) {
+      /** @var Product $product */
+      $product = $item['product'];
+      $amount = $item['amount'];
+
+      $mercadoPagoPreferenceItems[] = new MercadoPagoItem([
+        'id' => "{$payment->id}_{$product->id}",
+        'title' => $product->title,
+        'quantity' => $amount,
+        'unit_price' => $product->price,
+        'currency_id' => config('app.payment_currency')
+      ]);
+    }
+
+    $mercadoPagoPayer = new MercadoPagoPayer([
+      'name' => $user->name,
+      'email' => $user->email
+    ]);
+
+    $preference = new MercadoPagoPreference([
+      'items' => $mercadoPagoPreferenceItems,
+      'payer' => $mercadoPagoPayer,
+      'notification_url' => $this->notificationUrl
+    ]);
+
+    $preferenceAttributes = $preference->getAttributes();
+
+    return response()->json([
+      'id' => $payment->id,
+      'preference_id' => $preferenceAttributes['id'],
+      'link' => $preferenceAttributes['init_point']
+    ]);
   }
 
   public function handleNotification(Request $request): Response
