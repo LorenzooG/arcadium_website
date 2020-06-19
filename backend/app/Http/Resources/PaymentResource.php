5c6f280727d71efa1bd5ad97bd17c29a6da9aace
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-19 05:45:59 -0300

    fix: fix payment resource with new props

--- backend/app/Http/Resources/PaymentResource.php
+++ backend/app/Http/Resources/PaymentResource.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Resources;
 
+use App\Payment;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * Class PaymentResource
+ *
+ * @property Payment $resource
+ *
+ * @package App\Http\Resources
+ */
 final class PaymentResource extends JsonResource
 {
   /**
@@ -16,17 +24,21 @@ final class PaymentResource extends JsonResource
   public final function toArray($request)
   {
     return [
-      "id" => $this->id,
-      "user" => new UserResource($this->user),
-      "products" => PurchasedProductResource::collection($this->products()->get()),
-      "user_name" => $this->user_name,
-      "total_price" => floatval($this->total_price),
-      "delivered" => $this->delivered,
-      "payment_response" => $this->payment_response,
-      "payment_type" => $this->payment_type,
-      "payment_raw_response" => $this->payment_raw_response,
-      "created_at" => $this->created_at,
-      "updated_at" => $this->updated_at
+      'id' => $this->resource->id,
+      'user_name' => $this->resource->user_name,
+      'total_paid' => $this->resource->total_paid,
+      'total_price' => $this->resource->total_price,
+      'is_delivered' => $this->resource->is_delivered,
+      'origin_ip_address' => $this->resource->origin_address,
+      'payment_method' => $this->resource->payment_method,
+      'products' => route('payments.products.index', [
+        'payment' => $this->resource->id
+      ]),
+      'user' => route('users.show', [
+        'user' => $this->resource->user_id
+      ]),
+      'created_at' => $this->resource->created_at,
+      'updated_at' => $this->resource->updated_at,
     ];
   }
 }
