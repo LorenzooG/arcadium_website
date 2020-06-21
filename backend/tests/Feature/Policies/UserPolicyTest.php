f9afb84f3f345a475042b847413c5160e7621156
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-21 17:32:52 -0300

    test: test user policy

---
+++ backend/tests/Feature/Policies/UserPolicyTest.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace Tests\Feature\Policies;
+
+use App\Role;
+use App\User;
+use App\Utils\Permission;
+use Tests\TestCase;
+
+final class UserPolicyTest extends TestCase
+{
+  public function testShouldCanUpdateUserWhenHavePermissionUpdateAnyUser()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->create();
+    $user->roles()->save(factory(Role::class)->create([
+      'permission_level' => Permission::UPDATE_ANY_USER
+    ]));
+
+    $this->assertTrue($user->can('update', User::class));
+  }
+
+  public function testShouldCanDeleteUserWhenHavePermissionDeleteAnyUser()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->create();
+    $user->roles()->save(factory(Role::class)->create([
+      'permission_level' => Permission::DELETE_ANY_USER
+    ]));
+
+    $this->assertTrue($user->can('delete', User::class));
+  }
+
+  public function testShouldCanRestoreUserWhenHavePermissionRestoreAnyUser()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->create();
+    $user->roles()->save(factory(Role::class)->create([
+      'permission_level' => Permission::RESTORE_ANY_USER
+    ]));
+
+    $this->assertTrue($user->can('restore', User::class));
+  }
+
+  public function testShouldCanViewTrashedUsersWhenHavePermissionViewTrashedUsers()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->create();
+    $user->roles()->save(factory(Role::class)->create([
+      'permission_level' => Permission::VIEW_TRASHED_USERS
+    ]));
+
+    $this->assertTrue($user->can('viewTrashed', User::class));
+  }
+
+  public function testShouldCanUpdateSelfWhenHavePermissionUpdateUser()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->create();
+    $user->roles()->save(factory(Role::class)->create([
+      'permission_level' => Permission::UPDATE_USER
+    ]));
+
+    $this->assertTrue($user->can('update_self'));
+  }
+
+  public function testShouldCanDeleteSelfWhenHavePermissionDeleteUser()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->create();
+    $user->roles()->save(factory(Role::class)->create([
+      'permission_level' => Permission::DELETE_USER
+    ]));
+
+    $this->assertTrue($user->can('delete_self'));
+  }
+}
