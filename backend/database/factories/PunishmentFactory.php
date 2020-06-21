9a1af21d89f34dd9aec360148210c53e9b8acd3c
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-21 11:15:02 -0300

    test: test punishments controller

--- backend/database/factories/PunishmentFactory.php
+++ backend/database/factories/PunishmentFactory.php
@@ -1,12 +1,30 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
-use App\Model;
+use App\Punishment;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-$factory->define(Model::class, function (Faker $faker) {
-    return [
-        //
-    ];
+$factory->define(Punishment::class, function (Faker $faker) {
+  $punishedUserName = $this->faker->text(32);
+  $punishedBy = $this->faker->text(32);
+  $punishedUntil = $this->faker->unixTime;
+  $proof = $this->faker->text(240);
+  $reason = $this->faker->text(240);
+  $punishedAt = $this->faker->unixTime;
+
+  $punishmentDuration = Carbon::createFromTimestampMs($punishedAt)
+    ->diffInMilliseconds(Carbon::createFromTimestampMs($punishedUntil));
+
+  return [
+    'punished_user_name' => $punishedUserName,
+    'punished_at' => $punishedAt,
+    'punished_until' => $punishedUntil,
+    'punishment_duration' => $punishmentDuration,
+    'punished_by' => $punishedBy,
+    'reason' => $reason,
+    'proof' => $proof,
+  ];
 });
