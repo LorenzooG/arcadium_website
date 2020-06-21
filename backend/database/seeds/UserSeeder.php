1bb7e8124bb463954ebe6692b6869af078529c31
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-21 07:49:16 -0300

    test: seeding data

---
+++ backend/database/seeds/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Payment;
+use App\Post;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    factory(User::class, 100)->create()->each(function (User $user) {
+      $this->command->getOutput()->writeln("<comment>Generating user {$user->id} data...</comment>");
+
+      factory(Post::class, 160)->create([
+        'user_id' => $user->id
+      ]);
+
+      factory(Payment::class, 50)->create([
+        'user_id' => $user->id
+      ]);
+    });
+  }
+}
