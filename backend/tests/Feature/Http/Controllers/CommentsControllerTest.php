0982811fb78d101e1ae6a08c9d7d5d60fe71bed7
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-21 14:30:59 -0300

    refactor: refactor comments test

--- backend/tests/Feature/Http/Controllers/CommentsControllerTest.php
+++ backend/tests/Feature/Http/Controllers/CommentsControllerTest.php
@@ -6,7 +6,9 @@ use App\Comment;
 use App\Http\Controllers\CommentController;
 use App\Http\Requests\CommentStoreRequest;
 use App\Http\Requests\CommentUpdateRequest;
+use App\Post;
 use App\User;
+use Illuminate\Support\Collection;
 use JMac\Testing\Traits\AdditionalAssertions;
 use Tests\TestCase;
 
@@ -19,22 +21,17 @@ class CommentsControllerTest extends TestCase
    */
   public function testShouldShowCommentsWhenGetPostsComments()
   {
-    $title = $this->faker->title;
-    $description = $this->faker->text;
-
-    $commentContent = $this->faker->text(140);
-
+    /** @var User $user */
     $user = factory(User::class)->create();
 
-    $post = $user->posts()->create([
-      'title' => $title,
-      'description' => $description,
+    /** @var Post $post */
+    $post = factory(Post::class)->create([
+      'user_id' => $user->id
     ]);
 
-    $comment = $post->comments()->create([
-      'user_id' => $user->id,
+    factory(Comment::class, 15)->create([
       'post_id' => $post->id,
-      'content' => $commentContent
+      'user_id' => $user->id
     ]);
 
     $response = $this->getJson(route('posts.comments.index', [
@@ -43,17 +40,17 @@ class CommentsControllerTest extends TestCase
 
     $response->assertOk()
       ->assertJson([
-        'data' => [
-          [
+        'data' => Collection::make($post->comments()->paginate()->items())->map(function (Comment $comment) {
+          return [
             'id' => $comment->id,
             'content' => $comment->content,
             'created_by' => route('users.show', [
-              'user' => $user->id
+              'user' => $comment->user->id
             ]),
             'updated_at' => $comment->updated_at->toISOString(),
-            'created_at' => $comment->updated_at->toISOString(),
-          ],
-        ],
+            'created_at' => $comment->updated_at->toISOString()
+          ];
+        })->toArray()
       ]);
   }
 
@@ -62,31 +59,29 @@ class CommentsControllerTest extends TestCase
    */
   public function testShouldStoreCommentWhenPostPostsComments()
   {
-    $title = $this->faker->title;
-    $description = $this->faker->text;
-
-    $commentContent = $this->faker->text(140);
-
+    /** @var User $user */
     $user = factory(User::class)->state('admin')->create();
-
-    $post = $user->posts()->create([
-      'title' => $title,
-      'description' => $description
+    /** @var Post $post */
+    $post = factory(Post::class)->create([
+      'user_id' => $user->id
     ]);
+
+    $content = $this->faker->text(140);
 
     $response = $this->actingAs($user)->postJson(route('posts.comments.store', [
       'post' => $post->id
     ]), [
-      'content' => $commentContent
+      'content' => $content
     ]);
 
     $comments = Comment::query()
       ->where('id', $response->json('id'))
-      ->where('content', $commentContent)
+      ->where('content', $content)
       ->where('user_id', $user->id)
       ->where('post_id', $post->id)
       ->get();
 
+    /** @var Comment $comment */
     $comment = $comments->first();
 
     $this->assertCount(1, $comments);
@@ -132,17 +127,16 @@ class CommentsControllerTest extends TestCase
    */
   public function testShouldDeleteCommentsWhenDeleteComments()
   {
+    /** @var User $user */
     $user = factory(User::class)->state('admin')->create();
-
-    $post = $user->posts()->create([
-      'title' => $this->faker->title,
-      'description' => $this->faker->text,
+    /** @var Post $post */
+    $post = factory(Post::class)->create([
+      'user_id' => $user->id
     ]);
-
-    $comment = $post->comments()->create([
+    /** @var Comment $comment */
+    $comment = factory(Comment::class)->create([
       'user_id' => $user->id,
       'post_id' => $post->id,
-      'content' => $this->faker->text(140)
     ]);
 
     $response = $this->actingAs($user)->deleteJson(route('comments.delete', [
@@ -168,17 +162,16 @@ class CommentsControllerTest extends TestCase
    */
   public function testShouldUpdatePostWhenPutPosts()
   {
+    /** @var User $user */
     $user = factory(User::class)->state('admin')->create();
-
-    $post = $user->posts()->create([
-      'title' => $this->faker->title,
-      'description' => $this->faker->text,
+    /** @var Post $post */
+    $post = factory(Post::class)->create([
+      'user_id' => $user->id
     ]);
-
-    $comment = $post->comments()->create([
+    /** @var Comment $comment */
+    $comment = factory(Comment::class)->create([
       'user_id' => $user->id,
-      'post_id' => $post->id,
-      'content' => $this->faker->text(140)
+      'post_id' => $post->id
     ]);
 
     $content = $this->faker->text(140);
