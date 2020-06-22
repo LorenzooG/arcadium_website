c32d58fb695508f10c7891cc08531d1a260fc770
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-22 18:41:41 -0300

    test: test ProductPurchasedNotification.php

---
+++ backend/tests/Feature/Notifications/ProductPurchasedNotificationTest.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Tests\Feature\Notifications;
+
+use App\Notifications\ProductPurchasedNotification;
+use App\Product;
+use App\User;
+use Tests\TestCase;
+
+final class ProductPurchasedNotificationTest extends TestCase
+{
+  public function testShouldRenderProductPurchasedNotificationCorrectly()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->make();
+    $products = factory(Product::class, 15)->make();
+
+    $notification = new ProductPurchasedNotification($products);
+
+    $rendered = $notification->toMail($user)->render();
+
+    $this->assertStringContainsString($user->name, $rendered);
+
+    $products->each(function (Product $product) use ($rendered) {
+      $this->assertStringContainsString($product->title, $rendered);
+      $this->assertStringContainsString($product->pivot->amount, $rendered);
+    });
+  }
+
+  public function testShouldViaInProductPurchasedNotificationReturnArrayWithEmail()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->make();
+    $products = factory(Product::class, 15)->make();
+
+    $notification = new ProductPurchasedNotification($products);
+
+    $this->assertEquals(['mail'], $notification->via($user));
+  }
+
+  public function testShouldToArrayInProductPurchasedNotificationReturnArrayWithProductsAndUser()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->make();
+    $products = factory(Product::class, 15)->make();
+
+    $notification = new ProductPurchasedNotification($products);
+
+    $this->assertEquals([
+      'user' => $user,
+      'products' => $products
+    ], $notification->toArray($user));
+  }
+}
