44dd3e285ddfda8054fa54895685e76df9807d49
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-22 16:04:00 -0300

    feat: map testing routes in RouteServiceProvider.php

---
+++ backend/routes/testing.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register Testing routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which.
+| Enjoy testing your app!
+|
+*/
