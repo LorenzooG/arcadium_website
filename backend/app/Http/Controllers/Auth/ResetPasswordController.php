24c5ecde2a43c59e11de120646e00fc0b92bdd64
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-22 12:38:32 -0300

    feat: implement password reset features

---
+++ backend/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PasswordResetRequest;
+use App\Notifications\PasswordResetedNotification;
+use App\User;
+use Illuminate\Auth\Passwords\PasswordBroker;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
+
+class ResetPasswordController extends Controller
+{
+
+  /**
+   * Handle send password reset notification
+   *
+   * @param PasswordResetRequest $request
+   * @param mixed $token
+   * @return Response|JsonResponse
+   */
+  public function __invoke(PasswordResetRequest $request, $token)
+  {
+    $data = array_merge($request->only(['email']), [
+      'token' => $token
+    ]);
+
+    $result = $this->broker()->reset($data, function (User $user, $newPassword) {
+      $user->update([
+        'password' => $newPassword
+      ]);
+
+      $user->notify(new PasswordResetedNotification());
+    });
+
+    if (!($result instanceof User)) {
+      return response()->json([
+        'message' => $result
+      ], 400);
+    }
+
+    return response()->noContent();
+  }
+
+  /**
+   * Get the guard to be used during password rest
+   *
+   * @return StatefulGuard
+   */
+  public final function guard()
+  {
+    return Auth::guard('api');
+  }
+
+  /**
+   * Get the broker to be used during password reset.
+   *
+   * @return PasswordBroker
+   */
+  public final function broker()
+  {
+    return Password::broker('');
+  }
+
+}
