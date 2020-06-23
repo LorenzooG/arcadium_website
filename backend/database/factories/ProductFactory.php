8733495cfd772c1ac11f59957079d3c9996b8ce8
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-23 07:43:11 -0300

    feat: finish product paid notification markdown email.

--- backend/database/factories/ProductFactory.php
+++ backend/database/factories/ProductFactory.php
@@ -9,7 +9,7 @@ use Illuminate\Http\UploadedFile;
 
 $factory->define(Product::class, function (Faker $faker) {
   return [
-    'title' => $faker->text(72),
+    'title' => $faker->word,
     'description' => $faker->text(6000),
     'image' => UploadedFile::fake()->image($faker->text(8) . '.png', 100, 100),
     'price' => $faker->numberBetween(5, 15)
