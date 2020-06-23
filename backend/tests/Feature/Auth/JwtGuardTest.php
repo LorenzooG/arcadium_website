1ca296ed64fb60a7452ec7c71d47270eb775fb19
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-23 15:20:13 -0300

    test: test validate and append jwt guard

--- backend/tests/Feature/Auth/JwtGuardTest.php
+++ backend/tests/Feature/Auth/JwtGuardTest.php
@@ -41,4 +41,32 @@ final class JwtGuardTest extends TestCase
     $this->assertTrue(is_string($jwtToken));
     $this->assertTrue($repository->exists($user, $payload->token));
   }
+
+  public function testShouldValidateTokenAndIdAndSetUserWhenLogged()
+  {
+    /** @var JwtRepository $repository */
+    $repository = app(JwtRepository::class);
+
+    /** @var StatefulGuard $jwt */
+    $jwt = app(JwtGuard::class, [
+      'secret' => config('auth.jwt.secret'),
+      'algos' => config('auth.jwt.algos'),
+      'jwtRepository' => $repository
+    ]);
+
+    /** @var User $user */
+    $user = factory(User::class)->create();
+
+    $payload = JWT::decode($repository->create($user), config('auth.jwt.secret'), [config('auth.jwt.algos')]);
+
+    $payload = [
+      'id' => $user->id,
+      'token' => $payload->token
+    ];
+
+    $this->assertTrue($jwt->validate($payload));
+
+    $this->assertTrue($jwt->check());
+    $this->assertFalse($jwt->guest());
+  }
 }
