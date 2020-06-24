61d3aebaad460ffe3e3adce29c740f6376a1f477
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-24 13:58:22 -0300

    fix: remove avatar_url in user factory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -7,13 +7,11 @@ use App\User;
 use App\Utils\Permission;
 use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
-use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
   return [
     'name' => $faker->name,
     'user_name' => $faker->userName,
-    'avatar_url' => Str::random(12),
     'email' => $faker->unique()->safeEmail,
     'password' => $faker->password(8, 16),
   ];
