3e0c4ebd08589c624f50942b406bb839195ab024
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-25 10:35:40 -0300

    refactor: refactor route files and mock throttle requests

--- backend/tests/CreatesApplication.php
+++ backend/tests/CreatesApplication.php
@@ -4,8 +4,11 @@ namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Tests\Mocks\ThrottleRequestsMock;
 
 trait CreatesApplication
 {
@@ -16,7 +19,22 @@ trait CreatesApplication
    */
   public function createApplication()
   {
+    /** @var Application $app */
     $app = require __DIR__ . '/../bootstrap/app.php';
+
+    $app->singleton(\App\Http\Kernel::class, function (Application $app) {
+      $kernel = new \App\Http\Kernel($app, $app->make(Router::class));
+
+      $kernel->routeMiddleware['throttle'] = ThrottleRequestsMock::class;
+
+      foreach ($kernel->middlewareGroups['api'] as $key => $middleware) {
+        if (!Str::contains('throttle', $middleware)) continue;
+
+        $kernel->middlewareGroups['api'][$key] = 'throttle:1000,1';
+      }
+
+      return $kernel;
+    });
 
     $app->make(Kernel::class)->bootstrap();
 
