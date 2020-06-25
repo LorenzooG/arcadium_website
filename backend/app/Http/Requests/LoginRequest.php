fe9e034f532438a8e8080c443c019e0a0d74454b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-25 11:32:20 -0300

    refactor: rename AuthLoginRequest.php to LoginRequest.php

--- backend/app/Http/Requests/LoginRequest.php
+++ backend/app/Http/Requests/LoginRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-final class AuthLoginRequest extends FormRequest
+final class LoginRequest extends FormRequest
 {
   /**
    * Determine if the user is authorized to make this request.
