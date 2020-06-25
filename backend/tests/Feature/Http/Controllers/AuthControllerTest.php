b4f14bf5e301e6b14ea3b7945da0887611358c0c
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-25 11:40:13 -0300

    test: test auth controller

---
+++ backend/tests/Feature/Http/Controllers/AuthControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\User;
+use Tests\TestCase;
+
+final class AuthControllerTest extends TestCase
+{
+  public function testShouldLogin()
+  {
+    /** @var User $user */
+    $user = factory(User::class)->state('admin')->create();
+    $password = $this->faker->password(8, 16);
+
+    $response = $this->postJson(route('login'), [
+      'email' => $user->email,
+      'password' => $password
+    ]);
+
+    $response->assertOk()
+      ->assertJsonStructure([
+        'token'
+      ]);
+  }
+}
