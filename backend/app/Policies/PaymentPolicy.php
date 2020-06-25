58554b2d338cf071537ba6bf651d0b2de202cffd
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-25 09:54:27 -0300

    feat: add checkout permission policy

--- backend/app/Policies/PaymentPolicy.php
+++ backend/app/Policies/PaymentPolicy.php
@@ -2,23 +2,33 @@
 
 namespace App\Policies;
 
-use App\Payment;
 use App\User;
 use App\Utils\Permission;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PaymentPolicy
 {
-    use HandlesAuthorization;
+  use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param User $user
-     * @return mixed
-     */
-    public function view(User $user)
-    {
-      return $user->hasPermission(Permission::VIEW_PAYMENT);
-    }
+  /**
+   * Determine whether the user can view the model.
+   *
+   * @param User $user
+   * @return mixed
+   */
+  public function view(User $user)
+  {
+    return $user->hasPermission(Permission::VIEW_PAYMENT);
+  }
+
+  /**
+   * Determine whether the user can checkout a payment.
+   *
+   * @param User $user
+   * @return mixed
+   */
+  public function checkout(User $user)
+  {
+    return $user->hasPermission(Permission::CHECKOUT_PAYMENT);
+  }
 }
