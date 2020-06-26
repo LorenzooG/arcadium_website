6951324262a4367a807ded26b53c7a23e000d759
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-26 17:33:08 -0300

    refactor(web): remove reducer from _app

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
-import { wrapper } from '~/store'
-
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
-export default wrapper.withRedux(App)
+export default App
