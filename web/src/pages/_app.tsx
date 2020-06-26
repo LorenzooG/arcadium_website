2281c2c88de772d7e994171f2f01676726de4be5
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-26 13:48:07 -0300

    feat(web): implement redux

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
+import { wrapper } from '~/store'
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
-export default App
+export default wrapper.withRedux(App)
