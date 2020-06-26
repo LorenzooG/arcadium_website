112d60dd6af9a776be8287b36846bdc15f5820d1
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-26 17:30:25 -0300

    feat(web): redux with next

--- web/src/store/modules/index.ts
+++ web/src/store/modules/index.ts
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux'
 
+import { all } from 'redux-saga/effects'
+
+import postsReducer from './posts/reducer'
+import postsSagas from './posts/sagas'
+
+export const rootSaga = function* () {
+  return yield all([postsSagas])
+}
+
 export const rootReducer = combineReducers({
-  //
+  posts: postsReducer,
 })
