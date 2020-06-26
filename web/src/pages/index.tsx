d2760cd330b02d8fdcc1173020efb092b305859d
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-26 17:30:37 -0300

    feat(web): show posts with SSR

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+
 import { NextPage } from 'next'
 
+import { useSelector } from 'react-redux'
+
+import { END } from '@redux-saga/core'
+
+import { SagaStore, wrapper } from '~/store'
+import { actionFetchPosts } from '~/store/modules/posts/actions'
+
 const Index: NextPage = () => {
-  return <>Hello, world</>
+  const fuck = useSelector<any>(state => state.posts)
+
+  return <>{JSON.stringify(fuck, null, 2)}</>
 }
 
-export default Index
+Index.getInitialProps = async ({ store: _store }) => {
+  const store = _store as SagaStore
+
+  store.dispatch(actionFetchPosts())
+  store.dispatch(END)
+
+  await store.sagaTask?.toPromise()
+}
+
+export default wrapper.withRedux(Index)
