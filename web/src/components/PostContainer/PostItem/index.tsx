f581e6b9316088ba3da833fc3ed1239bb0f40aff
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-26 22:09:02 -0300

    feat(web): render posts to user

--- web/src/components/PostContainer/PostItem/index.tsx
+++ web/src/components/PostContainer/PostItem/index.tsx
@@ -1,17 +1,42 @@
 import React from 'react'
 
+import Link from 'next/link'
+
+import { FiUser } from 'react-icons/fi'
+
 import { Post } from '~/services/entities'
 
-import { Container } from './styles'
+import { Container, Header, Content, ContentText, Fade, Title } from './styles'
 
 interface Props {
   post: Post
 }
 
 export const PostItem: React.FC<Props> = ({ post }) => {
+  if (!post) return <h3>ERROR FUCKING BITCH</h3>
+
   return (
     <Container>
-      <div>{post.title}</div>
+      <Header>
+        <FiUser />
+
+        <div className={'info'}>
+          <div>Administration</div>
+          <span>{post.createdAt?.toDateString?.()}</span>
+        </div>
+      </Header>
+
+      <Content>
+        <Title>{post.title}</Title>
+        <ContentText
+          dangerouslySetInnerHTML={{
+            __html: post.description,
+          }}
+        />
+        <Fade>
+          <Link href={`/posts/${post.id}`}>Read more</Link>
+        </Fade>
+      </Content>
     </Container>
   )
 }
