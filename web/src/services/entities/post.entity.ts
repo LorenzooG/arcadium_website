9efc7bebd76018f291457b0226a60570722a3a1c
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-27 18:56:40 -0300

    refactor(web): extract creating post to a static function called new in post

--- web/src/services/entities/post.entity.ts
+++ web/src/services/entities/post.entity.ts
@@ -7,6 +7,25 @@ export class Post {
     public description: string,
     public createdBy: User,
     public createdAt: Date,
-    public updatedAt: Date
+    public updatedAt: Date,
+    public isComplete: boolean
   ) {}
+
+  public static new(post: any, isComplete = true) {
+    return new Post(
+      post.id,
+      post.title,
+      post.description,
+      new User(
+        post.created_by.id,
+        post.created_by.name,
+        post.created_by.user_name,
+        post.created_by.avatar,
+        post.created_by.email
+      ),
+      new Date(post.created_at),
+      new Date(post.updated_at),
+      isComplete
+    )
+  }
 }
