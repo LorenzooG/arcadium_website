ef99acc3bd94e2310594ab69bb840678ab1ceca5
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-26 21:20:07 -0300

    feat(web): use own style renderer

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
-import Document, { Head, Html, NextScript, Main } from 'next/document'
+import Document, {
+  Head,
+  Html,
+  NextScript,
+  Main,
+  DocumentContext,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-class AppDocument extends Document {
+interface Props {
+  stylesTag: React.Component
+}
+
+class AppDocument extends Document<Props> {
+  public static async getInitialProps({ renderPage }: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+
+    const props = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    )
+
+    const stylesTag = sheet.getStyleElement()
+
+    return { ...props, stylesTag }
+  }
+
   render() {
     return (
       <Html>
         <Head>
           <title>App</title>
+          {this.props.stylesTag}
         </Head>
         <body>
           <Main />
