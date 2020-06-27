327c7b78cac63f9973cb3116247dcb51a184177d
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-27 16:53:00 -0300

    feat(web): export post service class

--- web/src/services/crud/index.ts
+++ web/src/services/crud/index.ts
@@ -1,7 +1,7 @@
 import { AxiosInstance } from 'axios'
 import { createApi } from '~/services/api'
 
-export { postService } from './post.service'
+export { postService, PostService } from './post.service'
 
 export type ServiceClass<T> = {
   new (api: AxiosInstance): T
