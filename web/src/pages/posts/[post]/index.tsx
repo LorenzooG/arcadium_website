9a40b1f4767fa2f121f3832375e0d47d62ac9e66
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-06-28 12:50:44 -0300

    refactor(web): remove useFetcher from PostPage

--- web/src/pages/posts/[post]/index.tsx
+++ web/src/pages/posts/[post]/index.tsx
@@ -11,57 +11,12 @@ import { Container } from './styles'
 import { PostItem } from '~/components/PostContainer'
 import { wrapper } from '~/store'
 import { CommentList } from '~/components/CommentContainer'
+import { useFetcher } from '~/utils'
 
 interface Props {
   postId: string
   initialData?: Post
   comments?: Comment[]
-}
-
-interface Settings<F, D> {
-  fetcher: F
-  initialData: D
-}
-
-interface ReturnData<D> {
-  data: D | null
-  error: Error | null
-  loading: boolean
-}
-
-function useFetcher<D, F extends () => Promise<D>>({
-  fetcher: _fetcher,
-  initialData,
-}: Settings<F, D>): ReturnData<D> {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState<D | null>(null)
-  const [error, setError] = useState<Error | null>(null)
-
-  const fetcher = useCallback(_fetcher, [])
-
-  useEffect(() => {
-    async function fetchData() {
-      if (initialData) return
-
-      try {
-        setData(await fetcher())
-      } catch (error) {
-        setError(error)
-      }
-    }
-
-    fetchData().then(() => setLoading(false))
-  }, [fetcher, initialData])
-
-  if (initialData) {
-    return {
-      data: initialData,
-      loading: false,
-      error: null,
-    }
-  }
-
-  return { data, loading, error }
 }
 
 const PostPage: NextPage<Props> = ({ postId, initialData, comments }) => {
