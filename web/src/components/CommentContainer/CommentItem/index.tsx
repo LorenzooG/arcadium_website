925cf889e5d70b4a199b15c63f5140d8ddbe6e00
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-28 13:34:17 -0300

    style(web): style comments

--- web/src/components/CommentContainer/CommentItem/index.tsx
+++ web/src/components/CommentContainer/CommentItem/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
+import Link from 'next/link'
+
 import { Comment } from '~/services/entities'
+
+import { Container, Header, UserAvatar, Content } from './styles'
 
 interface Props {
   comment: Comment
@@ -8,8 +12,21 @@ interface Props {
 
 export const CommentItem: React.FC<Props> = ({ comment }) => {
   return (
-    <div>
-      <h1>{comment.content}</h1>
-    </div>
+    <Container>
+      <Header>
+        <UserAvatar src={comment.createdBy.avatar} />
+
+        <Link
+          href={'/profiles/[user]'}
+          as={`/profiles/${comment.createdBy.id}`}
+        >
+          <a>
+            <h4>{comment.createdBy.username}</h4>
+          </a>
+        </Link>
+      </Header>
+
+      <Content>{comment.content}</Content>
+    </Container>
   )
 }
