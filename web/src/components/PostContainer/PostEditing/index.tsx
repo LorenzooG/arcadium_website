8e2ffb6749a0c641923999e64d9f56cb3ff4ed0b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-06-28 14:47:16 -0300

    style(web): add edit post style

--- web/src/components/PostContainer/PostEditing/index.tsx
+++ web/src/components/PostContainer/PostEditing/index.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import { Post } from '~/services/entities'
 
-import { Container } from './styles'
+import {
+  Container,
+  Form,
+  Input,
+  Submit,
+  Label,
+  InputGroup,
+  TextBox,
+} from './styles'
 
 interface Props {
   post: Post
 }
 
-export const PostEditing: React.FC<Props> = () => {
-  return <Container />
+export const PostEditing: React.FC<Props> = ({ post }) => {
+  const titleRef = useRef<HTMLInputElement>(null)
+  const descriptionRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleOnSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handleOnSubmit}>
+        <InputGroup>
+          <Label htmlFor={'title'}>Title</Label>
+          <Input name={'title'} value={post.title} ref={titleRef} />
+        </InputGroup>
+
+        <InputGroup>
+          <Label htmlFor={'title'}>Description</Label>
+          <TextBox value={post.description} ref={descriptionRef} />
+        </InputGroup>
+
+        <Submit>FINISH</Submit>
+      </Form>
+    </Container>
+  )
 }
